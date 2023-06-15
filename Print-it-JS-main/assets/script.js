98c667b92759af0f4fc3c8779d79cd2f2e74dd46
Omar-Belalami <omarbelalami@yahoo.com>
2023-06-15 21:02:06 +0200

    creation carousel js

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,54 +1,63 @@
 const slidesData = [
 	{
-	  "image": "slide1.jpg",
-	  "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+	  image: "slide1.jpg",
+	  tagLine: "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-	  "image": "slide2.jpg",
-	  "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	  image: "slide2.jpg",
+	  tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-	  "image": "slide3.jpg",
-	  "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	  image: "slide3.jpg",
+	  tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-	  "image": "slide4.png",
-	  "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+	  image: "slide4.png",
+	  tagLine: "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
   ];
   
   let slideIndex = 0;
-  const slides = document.querySelectorAll('#banner .banner-img');
-  const dots = document.querySelectorAll('#banner .dot');
-
-  /**
- * Affiche la diapositive spécifiée par index
- * @param {number} currentIndex - L'index de la diapositive que on va afficher
- */
+  const slidesContainer = document.getElementById('banner');
+  const dotsContainer = document.createElement('div');
+  dotsContainer.classList.add('dots');
+  slidesContainer.appendChild(dotsContainer);
+  const dots = [];
   
-  function showSlide(currentIndex) {
-	for (let i = 0; i < slides.length; i++) {
-	  slides[i].style.display = 'none'; // Don't show any pictures
-	  dots[i].classList.remove('dot_selected'); // Don't show the class "dot_selected"
-	} 
-
-	slides[currentIndex].style.display = 'block'; //Affiche limage correspondant à l'index courant
-	dots[currentIndex].classList.add('dot_selected'); //Applique la class "dot_selected" uniquement au "dot" correspondant a l'image.
+  slidesData.forEach((slide, index) => {
+	const dot = document.createElement('div');
+	dot.classList.add('dot');
+	dotsContainer.appendChild(dot);
+	dots.push(dot);
+	dot.addEventListener('click', function () {
+	  showSlide(index);
+	});
+  });
+  
+  function showSlide(index) {
+	slideIndex = index;
+	slidesContainer.style.backgroundImage = `url(./assets/images/slideshow/${slidesData[slideIndex].image})`;
+	dots.forEach((dot, dotIndex) => {
+	  if (dotIndex === slideIndex) {
+		dot.classList.add('dot_selected');
+	  } else {
+		dot.classList.remove('dot_selected');
+	  }
+	});
   }
   
   function changeSlide(direction) {
-	slideIndex = (slideIndex + direction + slides.length) % slides.length;
+	slideIndex = (slideIndex + direction + slidesData.length) % slidesData.length;
 	showSlide(slideIndex);
-  } // Fonction pour changer l'image en fonction de la direction specifiée. (on utilise "slides.lenght" pour selectioner toutes les slides present et "%"" pour faire un sort que le switch de des images ne termine jamais.)
+  }
   
-  document.querySelector('#banner .arrow_left').addEventListener('click', function() {
+  document.querySelector('#banner .arrow_left').addEventListener('click', function () {
 	changeSlide(-1);
-  }); //Change l'image lorsque le bouton à gauche est cliqué
+  });
   
-  document.querySelector('#banner .arrow_right').addEventListener('click', function() {
+  document.querySelector('#banner .arrow_right').addEventListener('click', function () {
 	changeSlide(1);
-  }); //Change l'image lorsque le bouton à droit est cliqué
+  });
   
- 
   showSlide(slideIndex);
-  // Affiche l'image initiale
+  
